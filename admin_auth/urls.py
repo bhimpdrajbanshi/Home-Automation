44cb73683805f3e8565044a76e75dbcebcb4ae9f
bhimprasadrajbanshi <bhimpdraj123@gmail.com>
2025-10-14 20:31:45 +0545

    update static file

--- admin_auth/urls.py
+++ admin_auth/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('', include('controller.urls')),
+    path('ll/', include('controller.urls')),
 ]
 
 
