f59c967a5d3699480c7fe21510341358a332a449
bhimprasadrajbanshi <bhimpdraj123@gmail.com>
2025-10-14 20:45:54 +0545

    update static file

--- admin_auth/settings.py
+++ admin_auth/settings.py
@@ -116,15 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic will put all static files
+
+# No STATICFILES_DIRS needed if you use only app-level static folders
+# STATICFILES_DIRS = []  # optional
+
 MEDIA_URL = '/media/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
